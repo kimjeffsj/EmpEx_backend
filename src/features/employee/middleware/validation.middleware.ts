f335263ebcf905db2a7f1fe61e5b434c08876f8f
kimjeffsj <kim.jeffsj@gmail.com>
2025-01-17 20:20:06 -0800

    feat: updateEmployee validation added

--- src/features/employee/middleware/validation.middleware.ts
+++ src/features/employee/middleware/validation.middleware.ts
@@ -10,14 +10,14 @@ export const validateCreateEmployee = [
     .notEmpty()
     .withMessage("SIN number is required")
     .matches(/^\d{9}$/) // 9 digits numbers
-    .withMessage("유효한 SIN 번호 형식이 아닙니다"),
+    .withMessage("Invalid SIN number format"),
   body("email")
     .trim()
     .isEmail()
-    .withMessage("유효한 이메일 주소를 입력해주세요"),
-  body("dateOfBirth").isISO8601().withMessage("Type Valid Date of Birth"), // "2024-03-15" "2024-03-15T09:30:00Z" "2024-03-15T09:30:00.000Z" format
+    .withMessage("Please enter a valid email address"),
+  body("dateOfBirth").isISO8601().withMessage("Enter a Valid Date of Birth"), // "2024-03-15" "2024-03-15T09:30:00Z" "2024-03-15T09:30:00.000Z" format
   body("payRate").isFloat({ min: 17.5 }).withMessage("Minimum wage is $17.50"),
-  body("startDate").isISO8601().withMessage("Type Valid Start Date"), // "2024-03-15" "2024-03-15T09:30:00Z" "2024-03-15T09:30:00.000Z" format
+  body("startDate").isISO8601().withMessage("Enter a Valid Start Date"), // "2024-03-15" "2024-03-15T09:30:00Z" "2024-03-15T09:30:00.000Z" format
 
   // Validation process
   (req: Request, res: Response, next: NextFunction) => {
@@ -27,6 +27,46 @@ export const validateCreateEmployee = [
         code: "VALIDATION_ERROR",
         message: "Validation failed",
         detail: errors.array(),
+      });
+    }
+    next();
+  },
+];
+
+export const validateUpdateEmployee = [
+  // Optional field validation
+  body("firstName")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("First Name cannot be empty"),
+  body("lastName")
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage("Last Name cannot be empty"),
+  body("email")
+    .optional()
+    .trim()
+    .isEmail()
+    .withMessage("Enter a valid email address"),
+  body("payRate")
+    .optional()
+    .isFloat({ min: 17.5 })
+    .withMessage("Minimum wage is $17.50"),
+  body("resignedDate")
+    .optional()
+    .isISO8601()
+    .withMessage("Enter a Valid Resigned Date"),
+
+  // Validation process
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        code: "VALIDATION_ERROR",
+        message: "Validation Failed",
+        details: errors.array(),
       });
     }
     next();
