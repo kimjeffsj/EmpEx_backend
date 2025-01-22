42744e96671102988c15f30aa3f3934ed42b8539
kimjeffsj <kim.jeffsj@gmail.com>
2025-01-21 23:10:33 -0800

    feat: PayPeriod entity added

---
+++ src/entities/PayPeriod.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+
+export enum PayPeriodType {
+  FIRST_HALF = "FIRST_HALF", // 1 - 15
+  SECOND_HALF = "SECOND_HALF", // 16 - 31
+}
+
+export enum PayPeriodStatus {
+  PENDING = "PENDING",
+  PROCESSING = "PROCESSING",
+  COMPLETED = "COMPLETED",
+}
+
+@Entity("pay_periods")
+export class PayPeriod {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column("date")
+  startDate: Date;
+
+  @Column("date")
+  endDate: Date;
+
+  @Column({
+    type: "enum",
+    enum: PayPeriodType,
+  })
+  periodType: PayPeriodType;
+
+  @Column({
+    type: "enum",
+    enum: PayPeriodStatus,
+    default: PayPeriodStatus.PENDING,
+  })
+  status: PayPeriodStatus;
+
+  @CreateDateColumn({ type: "timestamptz" })
+  createdAt: Date;
+
+  @CreateDateColumn({ type: "timestamptz" })
+  updatedAt: Date;
+}
