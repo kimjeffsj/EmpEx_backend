bcd58fa4dc3ac706551ba068b147556eb7893913
kimjeffsj <kim.jeffsj@gmail.com>
2025-01-25 13:46:45 -0800

    feat: auth.types added

---
+++ src/shared/types/auth.types.ts
@@ -0,0 +1,24 @@
+export interface LoginDto {
+  email: string;
+  password: string;
+}
+
+export interface TakenPayload {
+  id: number;
+  email: string;
+  role: string;
+  employeeId?: number;
+}
+
+export interface AuthResponse {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    id: number;
+    email: string;
+    firstName: string;
+    lastName: string;
+    role: string;
+    employeeId?: number;
+  };
+}
